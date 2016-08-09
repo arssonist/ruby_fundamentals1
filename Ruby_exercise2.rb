cd7eda9868a232829269b611bc7a606aef9f3479
Chris <chrisdielschnieder@MacBook-Pro-3.local>
2016-08-09 14:55:44 -0400

    "Commit of ruby_1 assignments"

---
+++ Ruby_exercise2.rb
@@ -0,0 +1,15 @@
+#Q1
+def price_of_meal(price)
+tip = price * 0.2
+puts tip
+end
+price_of_meal(50)
+
+#Q2
+"Buffalo " + 66.to_s
+
+#Q3
+puts "#{45628 * 7839}"
+
+#Q4
+puts (10 < 20 && 30 < 20) || !(10 == 11)
