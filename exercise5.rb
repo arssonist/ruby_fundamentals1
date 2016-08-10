235753b92c93d4936bbd51d911ad503738234040
Your Name <arssonist@yahoo.com>
2016-08-10 14:43:42 -0400

    Excercise 5 ruby part 2

---
+++ exercise5.rb
@@ -0,0 +1,15 @@
+def converter(fahrenhiet)
+  celsius =(fahrenhiet - 32) * 5/9
+  (celsius.to_i)
+p "#{fahrenhiet} farenheit is equal to #{celsius} celsius"
+end
+
+converter(100
+)
+
+
+  #
+  # def calculator(number1, number2)
+  #     return(number1 + number2)
+  #   end
+  # puts  calculator(2,8)
