ec2f574bbcaf93731d1dac063882ad0b60c85c90
Your Name <arssonist@yahoo.com>
2016-08-10 16:07:45 -0400

    Excercis 6 third commite

--- exercise6.rb
+++ exercise6.rb
@@ -1,11 +1,21 @@
 
 #Q1
-grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+# grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 
 # grocery_list.each do |x|
 #   puts "* #{x}"
 # end
-# #Q2 (labeded as 1)
+#Q2 (labeded as 1)
 
-puts grocery_list << "rice"
+# grocery_list << "rice"
+
+#Q3
+def list_of_groceries
+  grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+  grocery_list.each do |x|
+    puts "* #{x}"
+  end
+end
+
+list_of_groceries
--- scratch.rb
+++ scratch.rb
@@ -1,8 +1,1 @@
-class Application
-  def initialize(name)
-    @name = name
-  end
-end
 
-class MyApp < Application
-end
