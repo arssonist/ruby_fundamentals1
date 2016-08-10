3abbd67800a718dfb1962255058ecd47613a8c76
Your Name <arssonist@yahoo.com>
2016-08-10 16:52:09 -0400

    Excersice 6 fifth comitt

--- exercise6.rb
+++ exercise6.rb
@@ -6,11 +6,11 @@
 # grocery_list.each do |x|
 #   puts "* #{x}"
 # end
+
 # Q2 (labeded as 1)
 
 # grocery_list << "rice"
 
-#Q3
 # def list_of_groceries
 #   grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 #   grocery_list.each do |x|
@@ -20,10 +20,31 @@
 
 
 
-#Q4
+#Q3
+
+# grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+#
+# grocery_list << "rice"
+
+
+# puts grocery_list
+#
+# puts grocery_list.length
+
+ #Q4
+
+ # puts grocery_list.include? "bananas"
+
+ #Q5
+
+ # puts grocery_list[1]
+
+#Q6
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 grocery_list << "rice"
 
-puts grocery_list.length
+grocery_list.sort.each do |x|
+  puts "* #{x}"
+end
