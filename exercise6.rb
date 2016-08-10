4084d61d54bb80a22a8b2b60a251921dabd66dc4
Your Name <arssonist@yahoo.com>
2016-08-10 16:20:06 -0400

    Excercise 6 fourth commit

--- exercise6.rb
+++ exercise6.rb
@@ -6,16 +6,24 @@
 # grocery_list.each do |x|
 #   puts "* #{x}"
 # end
-#Q2 (labeded as 1)
+# Q2 (labeded as 1)
 
 # grocery_list << "rice"
 
 #Q3
-def list_of_groceries
-  grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
-  grocery_list.each do |x|
-    puts "* #{x}"
-  end
-end
+# def list_of_groceries
+#   grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+#   grocery_list.each do |x|
+#     puts "* #{x}"
+#   end
+# end
 
-list_of_groceries
+
+
+#Q4
+
+grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+grocery_list << "rice"
+
+puts grocery_list.length
